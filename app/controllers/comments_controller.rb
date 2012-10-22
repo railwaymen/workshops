eaf58c0e0a8a1e6fe9ebe5f00d547dde8c4a00e8
kbackowski <kbackowski@gmail.com>
2012-10-22 11:38:44 +0200

    Updated comments form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@ class CommentsController < ApplicationController
 
   def create
     @comment = current_user.comments.build(params[:comment])
+    @comment.post = Post.find(params[:post_id])
     if @comment.save
       redirect_to :back, :notice => 'Dodano komentarz'
     end
